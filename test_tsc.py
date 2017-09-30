e987296684c030864d77a6e5bd0199b24d9bb717
baixiao <baixiao@codoon.com>
2017-09-30 18:40:20 +0800

    1.0 softmax

---
+++ test_tsc.py
@@ -0,0 +1,107 @@
+# -*- coding:utf-8 -*-
+__author__ = 'baixiao'
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import argparse
+# import skimage
+# from skimage import transform
+# from skimage import data
+import matplotlib.pyplot as plt
+from PIL import Image
+import random
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(im_height, im_width, 1).astype(np.uint8)
+
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory)
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) for f in os.listdir(label_directory) if f.endswith(".ppm")]
+        for f in file_names:
+            # images.append(skimage.data.imread(f))
+            imgfile = Image.open(f).convert('L')
+            images.append(load_image_into_numpy_array(imgfile.resize((28, 28), Image.NEAREST)))
+            label = np.zeros(62, dtype=np.int)
+            label[int(d)] = 1
+            # print label
+            labels.append(label)
+    return images, labels
+
+
+'''
+ROOT_PATH = "/Users/baixiao/Go/src/github.com/baixiaoustc/tensorflow_pytest"
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+
+images_array = np.array(images)
+labels_array = np.array(labels)
+
+# 定义模型
+x = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28])
+y = tf.placeholder(dtype=tf.int64, shape=[None])
+images_flat = tf.contrib.layers.flatten(x)
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+correct_pred = tf.equal(tf.argmax(logits, 1), y)
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", correct_pred)
+
+
+# 训练模型
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for i in range(2001):
+        # print('EPOCH', i)
+        _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images, y: labels})
+        if i % 10 == 0:
+            print("accuracy_val: ", accuracy_val)
+        # print('DONE WITH EPOCH')
+
+
+# 运行模型
+# Pick 10 random images
+sample_indexes = random.sample(range(len(images)), 10)
+sample_images = [images[i] for i in sample_indexes]
+sample_labels = [labels[i] for i in sample_indexes]
+
+# Run the "predicted_labels" op.
+predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
+
+# Print the real and predicted labels
+print(sample_labels)
+print(predicted)
+
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2,1+i)
+    plt.axis('off')
+    color='green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i])
+
+plt.show()
+'''
---
+++ tsc_1.0_softmax.py
@@ -0,0 +1,185 @@
+# -*- coding:utf-8 -*-
+__author__ = 'baixiao'
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import argparse
+# import skimage
+# from skimage import transform
+# from skimage import data
+import matplotlib.pyplot as plt
+from PIL import Image
+import random
+from test_tsc import load_data
+
+ROOT_PATH = "/Users/baixiao/Go/src/github.com/baixiaoustc/tensorflow_pytest"
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+
+class TrainData:
+    # _loop = 0
+    _images = []
+    _labels = []
+
+    def __init__(self):
+        self._images, self._labels = load_data(train_data_directory)
+        print("TrainData", len(self._images), len(self._labels))
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    def next_batch(self, limit=100):
+        # index=random.sample(range(len(train.images)/limit), 1)[0]
+        # print(index)
+        i = self._images#[index*limit:(index+1)*limit]
+        l = self._labels#[index*limit:(index+1)*limit]
+        # self._loop += 1
+        return i, l
+
+class TestData:
+    # _loop = 0
+    _images = []
+    _labels = []
+
+    def __init__(self):
+        self._images, self._labels = load_data(test_data_directory)
+        print("TestData", len(self._images), len(self._labels))
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    def next_batch(self, limit=100):
+        # index=random.sample(range(len(train.images)/limit), 1)[0]
+        # print(index)
+        i = self._images#[index*limit:(index+1)*limit]
+        l = self._labels#[index*limit:(index+1)*limit]
+        # self._loop += 1
+        return i, l
+
+
+train = TrainData()
+test = TestData()
+
+BATCH_NUM = 4575
+TAEGET_NUM = 62
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, TAEGET_NUM])
+# weights W[784, 10]   784=28*28
+W = tf.Variable(tf.zeros([784, TAEGET_NUM]))
+# biases b[10]
+b = tf.Variable(tf.zeros([TAEGET_NUM]))
+
+# flatten the images into a single line of pixels
+# -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
+XX = tf.reshape(X, [-1, 784])
+
+# The model
+# Y = tf.nn.softmax(tf.matmul(XX, W) + b)
+Y = tf.matmul(XX, W) + b
+print(X, XX, Y)
+
+# loss function: cross-entropy = - sum( Y_i * log(Yi) )
+#                           Y: the computed output vector
+#                           Y_: the desired output vector
+
+# cross-entropy
+# log takes the log of each element, * multiplies the tensors element by element
+# reduce_mean will add all the components in the tensor
+# so here we end up with the total cross-entropy for all images in the batch
+# cross_entropy = -tf.reduce_mean(Y_ * tf.log(Y)) * BATCH_NUM * TAEGET_NUM * 1.0  # normalized for batches of 100 images,
+                                                          # *10 because  "mean" included an unwanted division by 10
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y_, logits=Y))
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+predict = tf.argmax(Y, 1)
+correct_prediction = tf.equal(predict, tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# training, learning rate = 0.005
+train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# You can call this function in a loop to train the model, 100 images at a time
+def training_step(i, update_test_data, update_train_data):
+    batch_X, batch_Y = train.next_batch(BATCH_NUM)
+    if len(batch_X) == 0:
+        return
+    # print(batch_X[0])
+    # print(batch_Y[0])
+    #
+    # fig = plt.figure(figsize=(10, 10))
+    # plt.subplot(5, 2, 1+i)
+    # plt.axis('off')
+    # plt.imshow(batch_X[0].reshape(28,28))
+    # plt.show()
+
+    # compute training values for visualisation
+    if update_train_data:
+        a, c = sess.run([accuracy, cross_entropy], feed_dict={X: batch_X, Y_: batch_Y})
+        print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c))
+
+    # compute test values for visualisation
+    if update_test_data:
+        test_X, test_Y = test.next_batch()
+        a, c = sess.run([accuracy, cross_entropy], feed_dict={X: test_X, Y_: test_Y})
+        print(str(i) + ": ********* epoch " + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
+
+    # the backpropagation training step
+    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
+
+for i in range(200+1):
+    training_step(i, i % 100 == 0, i % 10 == 0)
+
+# 运行模型
+# Pick 10 random images
+sample_indexes = random.sample(range(len(train.images)), 10)
+# print("sample_indexes", sample_indexes)
+sample_images = [train.images[i] for i in sample_indexes]
+sample_labels = [train.labels[i] for i in sample_indexes]
+
+# Run the "predicted_labels" op.
+predicted = sess.run([predict], feed_dict={X: sample_images})[0]
+
+# Print the real and predicted labels
+# print(sample_labels)
+print("predicted", predicted)
+
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = np.argmax(sample_labels[i])
+    # print(truth, sample_labels[i])
+    prediction = predicted[i]
+    plt.subplot(5, 2, 1+i)
+    plt.axis('off')
+    color = 'green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i].reshape(28,28))
+
+plt.show()
